9446b11aed6faddffcf37ee788a7368630b1a647
theboydontplaygames <52797054+theboydontplaygames@users.noreply.github.com>
2022-01-05 15:14:15 +0200

    initial

---
+++ Factorial Calculator.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace FactorialCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblFactorial = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtFactorial = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(67, 178);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(222, 178);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "e&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(64, 39);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(50, 13);
+            this.lblNumber.TabIndex = 2;
+            this.lblNumber.Text = "Number: ";
+            // 
+            // lblFactorial
+            // 
+            this.lblFactorial.AutoSize = true;
+            this.lblFactorial.Location = new System.Drawing.Point(64, 87);
+            this.lblFactorial.Name = "lblFactorial";
+            this.lblFactorial.Size = new System.Drawing.Size(53, 13);
+            this.lblFactorial.TabIndex = 3;
+            this.lblFactorial.Text = "Factorial: ";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(213, 31);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 4;
+            // 
+            // txtFactorial
+            // 
+            this.txtFactorial.Location = new System.Drawing.Point(213, 87);
+            this.txtFactorial.Name = "txtFactorial";
+            this.txtFactorial.ReadOnly = true;
+            this.txtFactorial.Size = new System.Drawing.Size(100, 20);
+            this.txtFactorial.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(353, 268);
+            this.Controls.Add(this.txtFactorial);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblFactorial);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Factorial Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblFactorial;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtFactorial;
+    }
+}
+
---
+++ Factorial Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactorialCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int factorial = 1;
+            int number;
+            int i;
+            
+
+            number = Convert.ToInt32(txtNumber.Text);
+           
+            for     (i = 1; i <= number; i++)
+            {
+                factorial = factorial * i;
+                txtFactorial.Text = factorial.ToString();
+               
+
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
